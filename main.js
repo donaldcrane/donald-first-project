edd0a7988fb667cd9d5c72a3cefe074c501ac0af
donaldcrane <61365997+donaldcrane@users.noreply.github.com>
2020-05-30 03:39:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+function Validation(){
+	var firstname = document.getElementById('firstname').value;
+	var lastname = document.getElementById('lastname').value;
+	var email = document.getElementById('email').value;
+	var password = document.getElementById('password').value;
+	var password2 = document.getElementById('password2').value;
+	var essay = document.getElementById('essay').value;
+	var error = document.getElementById('error');
+	var text;
+
+
+	error.style.padding = "10px";
+
+	if(firstname.length < 4){
+		text = "Name must be at least 4 characters";
+		error.innerHTML = text;
+		return false;
+	}else if(lastname.length < 4){
+		text = "Name must be at least 4 characters";
+		error.innerHTML = text;
+		return false;
+	}else if(password.length < 4){
+		text = "password too weak";
+		error.innerHTML = text;
+		return false;
+	}else if(password != password2){
+		text = "password do not match";
+		error.innerHTML = text;
+		return false;
+	}else if(essay < 25){
+		text = "Text too short!";
+		error.innerHTML = text;
+		return false;
+	}else if(email.indexOf("@") == -1 || email.length < 6){
+		text = "Enter a valid email";
+		error.innerHTML = text;
+		return false;
+	}
+	
+	return false;
+	
+	
+
+
+	
+}
+
+/*const init = function (){
+	document.getElementById('button-cancel').addEventListener('click', reset)
+	document.getElementById('button-send').addEventListener('click', send)
+
+}
+
+const reset = function(ev){
+	ev.preventDefault();
+	document.getElementById('form').reset();
+}
+const send = function(ev){
+	ev.preventDefault();
+	
+	
+
+	if (Validation){
+		document.getElementById('form').submit();
+
+	}else{
+
+	}
+
+
+}
+
+document.addEventListener('DOMContentLoaded', init)
+*/
